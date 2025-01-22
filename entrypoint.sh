c580ce0e4882adf0dca76fe9f420c84e9da1ffee
Samuel Jonathan JOSEPH <isteah.josephsamuel@gmail.com>
2025-01-22 08:58:03 -0500

    manage ressource

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,11 +6,7 @@
 # Collecter les fichiers statiques
 python manage.py collectstatic --noinput
 
-# Démarrer Celery (en arrière-plan)
-celery -A config worker --loglevel=info &
 
-# Démarrer le serveur Celery Beat
-celery -A config beat --loglevel=info &
 
 # Démarrer Gunicorn
 exec gunicorn --bind 0.0.0.0:8000 config.wsgi:application
